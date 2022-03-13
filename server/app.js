6c6eac137f542861e486e3c493e033baab9bdf9c
Fady-Alwazir <fady.alwazir@gmail.com>
2022-03-13 16:14:05 +0200

    update sign up handler

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 
 const { apiRoute, publicRoute } = require('./routes');
+const { notFound, serverError } = require('./controllers');
 
 const app = express();
 
@@ -13,4 +14,6 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 app.set('port', process.env.PORT || 4000);
 app.use(apiRoute);
 app.use(publicRoute);
+app.use(notFound);
+app.use(serverError);
 module.exports = app;
