3357c69e07f32e4f8ea6e26003db490e80f6a0a5
Fady-Alwazir <fady.alwazir@gmail.com>
2022-03-14 13:56:07 +0200

    get all home posts Relates #36

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,4 @@
-const { addPost, upvote, downvote } = require('../database/queries');
+const { addPost, upvote, downvote, homePosts } = require('../database/queries');
 
 const addPostHandler = (req, res, next) => {
   const { title, content, userId } = req.body;
@@ -16,4 +16,8 @@ const downvoteHandler = (req, res, next) => {
     .then(() => res.send()).catch((err) => next(err));
 };
 
-module.exports = { addPostHandler, upvoteHandler, downvoteHandler };
+const homePostsHandler = (req, res, next) => {
+  homePosts().then((data) => res.json(data.rows)).catch((err) => next(err));
+};
+
+module.exports = { addPostHandler, upvoteHandler, downvoteHandler, homePostsHandler };
---
+++ server/database/queries/posts/homePosts.js
@@ -0,0 +1,5 @@
+const dbconnection = require('../../config/connection');
+
+const homePosts = () => dbconnection.query('SELECT posts.id, posts.post_text, posts.post_date, posts.title, posts.vote, users.username, users.img FROM posts INNER JOIN users ON users.id = posts.user_id');
+
+module.exports = homePosts;
