472627326c737c5f4b7160d98587476b27d8d665
Fady-Alwazir <fady.alwazir@gmail.com>
2022-03-16 22:58:01 +0200

    handling adding post frontend Relates #78

--- public/js/home.js
+++ public/js/home.js
@@ -4,3 +4,22 @@ fetch('/homePosts').then((res) => res.json()).then((data) => {
     createPost(ele.id, ele.vote, ele.username, ele.title, ele.content, ele.img_post);
   });
 });
+
+const postForm = document.querySelector('.post-form');
+
+postForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = e.target.title.value;
+  const content = e.target.content.value.trim();
+  const img = e.target.img.value;
+  console.log(title, content, img);
+
+  // eslint-disable-next-line no-undef
+  fetchData({ title, content, img }, 'post', '/posts').then((data) => {
+    if (data.status !== 201) {
+      window.location.href = '/login';
+    } else {
+      window.location.href = '/';
+    }
+  });
+});
